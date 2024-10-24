5c7539a9dba5bef7ab089353e61c7a2cd68da183
Michal Svačina <msvacina@outlook.com>
2024-10-24 14:12:23 +0200

    feat: implemented fill-template command

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,11 @@ use keepass::{
 use clap::Parser;
 
 use arguments::Args;
-use models::{kp_entry::KpEntry, kp_group::KpGroup, kp_tree::KpTree, kp_error::KpError};
-use std::fs::File;
+use models::{kp_entry::KpEntry, kp_error::KpError, kp_group::KpGroup, kp_tree::KpTree};
+use regex::{self, Regex};
+use std::{
+    fs::{read_to_string, File}, io::{self, Write},
+};
 
 fn main() -> Result<(), DatabaseOpenError> {
     let args = Args::parse();
@@ -35,20 +38,18 @@ fn main() -> Result<(), DatabaseOpenError> {
     let keepass_tree = KpTree::new(loaded_database);
 
     match &args.command {
-        arguments::Commands::GetEntry { path } => {
-            match find_entry_by_path(&keepass_tree, &path) {
-                Ok(entry) => {
-                    println!("Entry detail: {:?}", entry);
-                },
-                Err(e) => println!("ERROR: {}", e),
+        arguments::Commands::GetEntry { path } => match find_entry_by_path(&keepass_tree, &path) {
+            Ok(entry) => {
+                println!("Entry detail: {:?}", entry);
             }
+            Err(e) => println!("ERROR: {}", e),
         },
         arguments::Commands::GetUsername { path } => {
             match find_username_by_path(&keepass_tree, &path) {
                 Ok(username) => println!("Found username: {}", username),
                 Err(e) => println!("ERROR: {}", e),
             }
-        },
+        }
         arguments::Commands::GetPassword { path } => {
             match find_password_by_path(&keepass_tree, &path) {
                 Ok(pass) => println!("Password for entry '{}': '{}'", path, pass),
@@ -56,11 +57,128 @@ fn main() -> Result<(), DatabaseOpenError> {
             }
         }
         arguments::Commands::FillTemplate { file_path } => {
-            todo!("Not implemented... yet!");
-        },
+            match fill_template(&keepass_tree, &file_path) {
+                Ok(_) => std::process::exit(0),
+                Err(e) => {
+                    println!("ERROR: Following errors found: \n{}", e);
+                    std::process::exit(1);
+                }
+            }
+            // Open file
+            // Find all regex matches
+            // Find all variables in keepass_tree
+            // - If any variable is missing, add it to ERROR list that will be printed out at the end
+            // If no errors were found, replace all regex matches with the variables and save the file
+        }
     }
 
     Ok(())
+}
+
+fn fill_template(kp_tree: &KpTree, file_path: &String) -> Result<bool, KpError> {
+    let mut variables: Vec<(String, String)> = Vec::new();
+    let mut errors: Vec<(String, KpError)> = Vec::new();
+
+    if let Ok(matches) = get_regex_matches(file_path) {
+        for match_tuple in matches {
+            match find_entry_by_path(kp_tree, match_tuple.1.as_str()) {
+                Ok(entry) => {
+                    // DEBUG: println!("Found entry for: {}", match_tuple.0);
+                    variables.push((match_tuple.0, entry.password.as_ref().unwrap().to_owned()));
+                }
+                Err(e) => {
+                    errors.push((match_tuple.0, e));
+                }
+            }
+        }
+    }
+
+    if variables.is_empty() {
+        println!("Variables: {:?}", variables);
+    }
+
+    if !errors.is_empty() {
+        println!("Errors: {:?}", errors);
+        return Err(KpError::TemplateVariablesNotFound(errors));
+    }
+
+    let _ = write_filled_template(file_path, variables);
+
+    return Ok(true);
+}
+
+
+fn write_filled_template(file_path: &String, variables: Vec<(String, String)>) -> io::Result<()> {
+    let file_contents = prepare_template_export(file_path, variables);
+    let mut file = File::create("output.yaml")?;
+    
+    for line in file_contents {
+        writeln!(file, "{}", line)?;
+    }
+
+    Ok(())
+}
+
+fn prepare_template_export(file_path: &String, variables: Vec<(String, String)>) -> Vec<String> {
+    let re = Regex::new(
+        r"\{\{\s*(?<search_path>[a-zA-Z\._0-9]{1,}):(?<entry_type>USERNAME|PASSWORD)\s*\}\}",
+    )
+    .unwrap();
+
+    let mut file_vec: Vec<String> = Vec::new();
+
+    {
+        for line in read_to_string(file_path).unwrap().lines() {
+            if let Some(matches) = re.captures(line) {
+                if let Some(full_match) = matches.get(0) {
+                    // find correct tuple
+                    let secret_tuple = variables
+                        .iter()
+                        .find(|tuple| tuple.0 == full_match.as_str().to_owned());
+
+                    file_vec.push(line.replace(full_match.as_str(), secret_tuple.unwrap().1.as_str()));
+                }
+            }
+            else {
+                file_vec.push(line.to_owned());
+            }
+        }
+    }
+
+    // println!("DEBUG: {:?}", file_vec);
+    file_vec
+}
+
+fn get_regex_matches(file_path: &String) -> Result<Vec<(String, String, String)>, KpError> {
+    let re = Regex::new(
+        r"\{\{\s*(?<search_path>[a-zA-Z\._0-9]{1,}):(?<entry_type>USERNAME|PASSWORD)\s*\}\}",
+    )
+    .unwrap();
+
+    // the tuple is <matched_string, search_path, entry_type>
+    let mut search_vec: Vec<(String, String, String)> = Vec::new();
+    {
+        for line in read_to_string(file_path).unwrap().lines() {
+            // DEBUG: ? println!("Line read: {}", line);
+            if let Some(matches) = re.captures(line) {
+                let mut regex_match: String = String::from("value");
+                if let Some(full_match) = matches.get(0) {
+                    // DEBUG: println!("Full match: {}", full_match.as_str());
+                    regex_match = full_match.as_str().to_owned();
+                }
+
+                // DEBUG: ? println!("Found match - Group:{}, Type: {}", &matches["search_path"], &matches["entry_type"]);
+                search_vec.push((
+                    regex_match,
+                    matches["search_path"].to_owned(),
+                    matches["entry_type"].to_owned(),
+                ));
+            }
+        }
+    }
+
+    // DEBUG: println!("Vec result: {:?}", search_vec);
+    Ok(search_vec)
 }
 
 // REVIEW: move to kp_tree.rs
@@ -79,7 +197,7 @@ fn create_group_node(group_ref: &Group, parent_group: &mut KpGroup) {
     for node in &group_ref.children {
         match node {
             Node::Group(g) => {
-                println!("Group '{}' added under: {}", g.name, group_ref.name);
+                // DEBUG: println!("Group '{}' added under: {}", g.name, group_ref.name);
                 let mut added_group = KpGroup::new();
                 create_group_node(&g, &mut added_group);
                 parent_group.groups.insert(g.clone().name, added_group);
@@ -88,7 +206,7 @@ fn create_group_node(group_ref: &Group, parent_group: &mut KpGroup) {
                 let title = e.get_title().unwrap_or("(no title)");
                 let user = e.get_username().unwrap_or("(no username)");
                 let pass = e.get_password().unwrap_or("(no password)");
-                println!("Adding '{}' to tree, under: {}", title, group_ref.name);
+                // DEBUG: println!("Adding '{}' to tree, under: {}", title, group_ref.name);
                 parent_group.entries.insert(
                     e.get_title().unwrap_or("(no title)").to_string(),
                     KpEntry::new(Some(user.to_string()), Some(pass.to_string())),
@@ -98,45 +216,41 @@ fn create_group_node(group_ref: &Group, parent_group: &mut KpGroup) {
     }
 }
 
-fn find_target_kp_group<'a>(kp_tree: &'a KpTree, splitted_search_path: &Vec<&str>) -> Result<&'a KpGroup, KpError> {
+fn find_target_kp_group<'a>(
+    kp_tree: &'a KpTree,
+    splitted_search_path: &Vec<&str>,
+) -> Result<&'a KpGroup, KpError> {
     let mut searched_group = &kp_tree.root_group;
-    
-    for part_path in splitted_search_path.iter().take(splitted_search_path.len() - 1) {
+
+    for part_path in splitted_search_path
+        .iter()
+        .take(splitted_search_path.len() - 1)
+    {
         if searched_group.groups.contains_key(*part_path) {
             searched_group = &searched_group.groups[*part_path];
-        }
-        else {
+        } else {
             return Err(KpError::GroupNotFound(splitted_search_path.join(".")));
         }
     }
-    
+
     Ok(searched_group)
 }
 
 fn find_password_by_path(kp_tree: &KpTree, search_path: &str) -> Result<String, KpError> {
     let splitted_search_path: Vec<&str> = search_path.split(".").collect();
     let searched_group = find_target_kp_group(kp_tree, &splitted_search_path)?;
-    
+
     let password_path = splitted_search_path.last().copied().unwrap();
     if searched_group.entries.contains_key(password_path) {
         // searched_group.entries.get(password_path).unwrap().password.as_ref().unwrap();
         match searched_group.entries.get(password_path) {
-            None => {
-                Err(KpError::EntryNotFound(search_path.to_string()))
+            None => Err(KpError::EntryNotFound(search_path.to_string())),
+            Some(entry) => match entry.password.as_ref() {
+                None => Err(KpError::PasswordNotFound(password_path.to_string())),
+                Some(password) => Ok(password.to_owned()),
             },
-            Some(entry) => {
-                match entry.password.as_ref() {
-                    None => {
-                        Err(KpError::PasswordNotFound(password_path.to_string()))
-                    },
-                    Some(password) => {
-                        Ok(password.to_owned())
-                    }
-                }
-            }
         }
-    }
-    else {
+    } else {
         Err(KpError::EntryNotFound(search_path.to_string()))
     }
 }
@@ -144,35 +258,24 @@ fn find_password_by_path(kp_tree: &KpTree, search_path: &str) -> Result<String, 
 fn find_entry_by_path<'a>(kp_tree: &'a KpTree, search_path: &str) -> Result<&'a KpEntry, KpError> {
     let splitted_search_path: Vec<&str> = search_path.split(".").collect();
     let searched_group = find_target_kp_group(kp_tree, &splitted_search_path)?;
-    
+
     let entry_path = splitted_search_path.last().copied().unwrap();
     if searched_group.entries.contains_key(entry_path) {
         // searched_group.entries.get(entry_path).unwrap().password.as_ref().unwrap();
         match searched_group.entries.get(entry_path) {
-            None => {
-                Err(KpError::EntryNotFound(search_path.to_string()))
-            },
-            Some(entry) => {
-                Ok(entry)
-            }
+            None => Err(KpError::EntryNotFound(search_path.to_string())),
+            Some(entry) => Ok(entry),
         }
-    }
-    else {
+    } else {
         Err(KpError::EntryNotFound(search_path.to_string()))
     }
 }
 
 fn find_username_by_path(kp_tree: &KpTree, search_path: &str) -> Result<String, KpError> {
     match find_entry_by_path(kp_tree, search_path) {
-        Ok(entry) => {
-            match &entry.username {
-                Some(username) => {
-                    Ok(username.clone())
-                },
-                None => {
-                    Err(KpError::UsernameNotFound(search_path.to_string()))
-                }
-            }
+        Ok(entry) => match &entry.username {
+            Some(username) => Ok(username.clone()),
+            None => Err(KpError::UsernameNotFound(search_path.to_string())),
         },
         Err(e) => Err(e),
     }
--- src/models/kp_error.rs
+++ src/models/kp_error.rs
@@ -6,6 +6,8 @@ pub enum KpError {
     EntryNotFound(String),
     PasswordNotFound(String),
     UsernameNotFound(String),
+    TemplateVariablesNotFound(Vec<(String, KpError)>),
+    NoVariablesInSourceFile(String),
 }
 
 impl fmt::Display for KpError {
@@ -15,6 +17,14 @@ impl fmt::Display for KpError {
             KpError::EntryNotFound(ref path) => write!(f, "Entry '{}' not found", path),
             KpError::PasswordNotFound(ref path) => write!(f, "Password '{}' not found for entry", path),
             KpError::UsernameNotFound(ref path) => write!(f, "Username '{}' not found for entry", path),
+            KpError::TemplateVariablesNotFound(ref vec) => {
+                let mut result = String::new();
+                for (variable, error) in vec {
+                    result.push_str(&format!("Template variable '{}' not found: {}\n", variable, error));
+                }
+                write!(f, "{}", result)
+            },
+            KpError::NoVariablesInSourceFile(ref file) => write!(f, "No variables found for replacing in template file {}", file),
         }
     }
 }
